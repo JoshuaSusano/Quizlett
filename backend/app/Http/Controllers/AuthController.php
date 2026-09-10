ca79b6c10a78c323e998a52246af98ebadb69888
Noclipsss <167722256+JoshuaSusano@users.noreply.github.com>
2026-09-10 17:57:46 +0800

    Handle authentication exceptions correctly

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class AuthController extends Controller
 {
