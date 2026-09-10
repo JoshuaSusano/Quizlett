c96a0668a72898df6a02e9eff7670cf1cbe41fba
Noclipsss <167722256+JoshuaSusano@users.noreply.github.com>
2026-09-10 15:44:22 +0800

    Prepare frontend and backend deployment

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [StartSession::class]);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
